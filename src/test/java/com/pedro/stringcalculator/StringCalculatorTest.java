25d807177802e2b1f77dd1e178f99ec8ddfb14fe
pedrovgs <pedrovicente.gomez@gmail.com>
2014-02-21 22:21:21 +0100

    Add two new tests related with parsing strings with only one number and different digits. RED

--- src/test/java/com/pedro/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/pedro/stringcalculator/StringCalculatorTest.java
@@ -21,6 +21,8 @@ public class StringCalculatorTest {
      */
 
     private static final String EMPTY_SET_OF_NUMBERS = "";
+    private static final String ONE_NUMBER_ONE_DIGIT_STRING = "1";
+    private static final String ONE_NUMBER_STRING = "11";
 
     /*
      * Test data
@@ -44,7 +46,24 @@ public class StringCalculatorTest {
     @Test
     public void shouldReturnZeroIfTheArgumentIsEmpty() {
         int result = stringCalculator.add(EMPTY_SET_OF_NUMBERS);
+
         assertEquals(0, result);
+    }
+
+    @Test
+    public void shouldReturnTheSameValueifTheArgumentContainsOnlyOneNumberWithOneDigit() {
+        int result = stringCalculator.add(ONE_NUMBER_ONE_DIGIT_STRING);
+
+        int expectedResult = Integer.parseInt(ONE_NUMBER_ONE_DIGIT_STRING);
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void shouldReturnTheSameValueIfTheArgumentContainisOnlyOneNumberWithMoreThanOneDigit() {
+        int result = stringCalculator.add(ONE_NUMBER_STRING);
+
+        int expectedResult = Integer.parseInt(ONE_NUMBER_ONE_DIGIT_STRING);
+        assertEquals(expectedResult, result);
     }
 
 }
