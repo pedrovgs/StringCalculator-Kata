a29cd696ee1bbeb3a81d338a9478628a36975a19
pedrovgs <pedrovicente.gomez@gmail.com>
2014-02-21 22:48:18 +0100

    Tests working with a not really easy implementation lets go to improve the code duality before continue working with more use cases. GREEN

--- src/main/java/com/pedro/stringcalculator/StringCalculator.java
+++ src/main/java/com/pedro/stringcalculator/StringCalculator.java
@@ -1,5 +1,10 @@
 package com.pedro.stringcalculator;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Main class of this application. StringCalculator is an application created to analyze one string, extract the
  * numeric values and return an integer that represents the sum of the numeric values. Some interesting conditions
@@ -28,19 +33,60 @@ public class StringCalculator {
     public int add(final String numbers) {
         int result = 0;
         if (!numbers.isEmpty()) {
+            List<Integer> numberList = new LinkedList<Integer>();
             if (onlyContainsOneNumber(numbers)) {
                 result = Integer.parseInt(numbers);
+            } else {
+                numberList = extractNumberList(numbers);
+            }
+            if (!numberList.isEmpty()) {
+                result = sumNumbers(numberList);
             }
         }
         return result;
     }
 
-    private boolean onlyContainsOneNumber(final String numbers) {
-        return numbers.matches(ONE_DIGIT_OR_MORE_REG_EX);
-    }
+
 
     /*
      * Auxiliary methods
      */
+
+    private boolean onlyContainsOneNumber(final String numbers) {
+        return numbers.matches(ONE_DIGIT_OR_MORE_REG_EX);
+    }
+
+    private List<Integer> extractNumberList(String numbers) {
+        List<Integer> result = new LinkedList<Integer>();
+        if (!numbers.isEmpty()) {
+            if (onlyContainsOneNumber(numbers)) {
+                int number = Integer.parseInt(numbers);
+                result.add(number);
+            } else {
+                result = getNumbersUsingRegEx(numbers);
+            }
+        }
+        return result;
+    }
+
+    private List<Integer> getNumbersUsingRegEx(String string) {
+        LinkedList<Integer> numbers = new LinkedList<Integer>();
+
+        Pattern p = Pattern.compile(ONE_DIGIT_OR_MORE_REG_EX);
+        Matcher m = p.matcher(string);
+        while (m.find()) {
+            int number = Integer.parseInt(m.group());
+            numbers.add(number);
+        }
+        return numbers;
+    }
+
+    private int sumNumbers(List<Integer> numberList) {
+        int sum = 0;
+        for (Integer num : numberList) {
+            sum += num;
+        }
+        return sum;
+    }
 
 }
