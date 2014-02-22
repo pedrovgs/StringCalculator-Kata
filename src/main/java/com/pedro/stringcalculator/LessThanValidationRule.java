84b4dc5dc47cec7d4acbe839b9e93920662bab93
pedrovgs <pedrovicente.gomez@gmail.com>
2014-02-22 13:51:49 +0100

    Implements LessThanValidationRule. GREEN

--- src/main/java/com/pedro/stringcalculator/LessThanValidationRule.java
+++ src/main/java/com/pedro/stringcalculator/LessThanValidationRule.java
@@ -22,7 +22,7 @@ class LessThanValidationRule implements NumberValidator.ValidationRule {
 
     @Override
     public boolean isValid(Integer number) {
-        return false;
+        return number < value;
     }
 
 }
