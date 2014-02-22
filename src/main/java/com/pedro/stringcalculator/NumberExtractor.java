b0cccd9fdf630d263eca619407bdae766eac27a3
pedrovgs <pedrovicente.gomez@gmail.com>
2014-02-22 12:47:36 +0100

    Implement the most important method of NumberExtractor based on regular expressions. GREEN

--- src/main/java/com/pedro/stringcalculator/NumberExtractor.java
+++ src/main/java/com/pedro/stringcalculator/NumberExtractor.java
@@ -2,6 +2,8 @@ package com.pedro.stringcalculator;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Class created to work as a number extractor. This class will receive a string with some numbers inside and will
@@ -11,11 +13,48 @@ import java.util.List;
  */
 class NumberExtractor {
 
-    public List<Integer> extract(final String stringToAnalyze) {
-        List<Integer> numbers = new LinkedList<Integer>();
-        if (stringToAnalyze != null && !stringToAnalyze.isEmpty()) {
+    /*
+     * Constants
+     */
 
+    private static final String ONE_DIGIT_OR_MORE_REG_EX = "-?\\d+";
+    private static final Pattern numberPattern = Pattern.compile(ONE_DIGIT_OR_MORE_REG_EX);
+
+    /*
+     * Public methods
+     */
+
+    /**
+     * Analyze the string passed as parameter searching numbers and return a list with all of them
+     *
+     * @param stringToAnalyze can contains different separators and numbers with more than one digit.
+     * @return a List<Integer> with the numbers found
+     */
+    public List<Integer> extract(final String stringToAnalyze) {
+        List<Integer> result = new LinkedList<Integer>();
+        if (stringToAnalyze != null && !stringToAnalyze.isEmpty()) {
+            result = getNumbersUsingRegEx(stringToAnalyze);
+        }
+        return result;
+    }
+
+    /*
+     * Auxiliary methods
+     */
+
+    private List<Integer> getNumbersUsingRegEx(String string) {
+        Matcher matcher = numberPattern.matcher(string);
+        return extractNumbersFromMatcher(matcher);
+    }
+
+    private LinkedList<Integer> extractNumbersFromMatcher(Matcher matcher) {
+        LinkedList<Integer> numbers = new LinkedList<Integer>();
+        while (matcher.find()) {
+            int number = Integer.parseInt(matcher.group());
+            numbers.add(number);
         }
         return numbers;
     }
+
+
 }
